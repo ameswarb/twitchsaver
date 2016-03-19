6499915adcf6be2de2ccd20fd8f56aaf470fc288
Alex Meswarb <alex.meswarb@gmail.com>
2016-03-19 14:24:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# twitchsaver
+
+[Check it out!](https://ameswarb.github.io/twitchsaver)
+
+Webpage that serves up a 3 x 2 grid of cathode tube TVs showing twitch.tv's featured streams. Designed to work with screensavers that load a webpage, such as [webviewscreensaver](https://github.com/liquidx/webviewscreensaver).
+
+## Installation
+
+Just clone this repo into any location that serves files over HTTP.
+
+## Usage
+
+Add `?theme=modern` to the end of the URL to remove the cathode tube theme and show 100% of the twitch streams.
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+var featuredRoute = 'https://api.twitch.tv/kraken/streams/featured?' +
+                    'limit=12&geo=US&lang=en&on_site=1';
+
+var pluckedChNames = function (channels) {
+  var output = [];
+
+  for (var i = 0; i < channels.length; i++) {
+    if (output.length < 6 && !channels[i].stream.channel.mature) {
+      output.push(channels[i].stream.channel.name);
+    }
+  }
+
+  return output;
+};
+
+var vertPos = function (i) {
+  if (i < 3) { return 'top'; } else { return 'bottom'; }
+};
+
+var horzPos = ['left', 'middle', 'right'];
+
+var streamRoute = [
+  'http://player.twitch.tv/?channel=',
+  '&amp;html5&amp;volume=0&amp;muted=true',
+];
+
+var embedTemplate = function (channel, order) {
+  return '<div class="video ' + vertPos(order) + ' ' + horzPos[order % 3] + '">' +
+           '<div class="overlay">' +
+             '<iframe src="' + streamRoute[0] + channel + streamRoute[1] + '"></iframe>' +
+           '</div>' +
+         '</div>';
+};
+
+var getParams = {};
+
+document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+  function decode(s) {
+    return decodeURIComponent(s.split('+').join(' '));
+  }
+
+  getParams[decode(arguments[1])] = decode(arguments[2]);
+});
+
+$(document).ready(function () {
+  if (getParams.theme) {
+    $('body').removeClass('tube');
+  }
+
+  $.get(featuredRoute, function (data) {
+    var myChannels = pluckedChNames(data.featured);
+
+    for (var i = 0; i < myChannels.length; i++) {
+      $('body').append(embedTemplate(myChannels[i], i));
+    }
+  });
+});
